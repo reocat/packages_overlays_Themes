3398803ab3a8649052687a11cf3ff0c46a70af3e
rezaadi0105 <rezaadipangestu5@gmail.com>
2023-08-15 06:31:29 +0000

    Themes: Data icon style [3/3]
    Overlays: Add data icons from iconify
    Credit: https://github.com/Mahmud0808/Iconify
    
    Change-Id: Ib9e0ab9efbb08771b71512b2e70cc4a207948290
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>
    Signed-off-by: helliscloser <mrumais@gmail.com>
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- themes.mk
+++ themes.mk
@@ -210,3 +210,19 @@ PRODUCT_PACKAGES += \
     MicroWaveWiFiOverlay \
     SharpWiFiOverlay \
     NothingDotWiFiOverlay
+
+# Data icons
+PRODUCT_PACKAGES += \
+    AuroraDataOverlay \
+    FaintUIDataOverlay \
+    GradiconDataOverlay \
+    LornDataOverlay \
+    PlumpyDataOverlay
+
+# Data Icon(prebuilt)
+PRODUCT_PACKAGES += \
+    IconDataFive \
+    IconDataFour \
+    IconDataOne \
+    IconDataThree \
+    IconDataTwo
