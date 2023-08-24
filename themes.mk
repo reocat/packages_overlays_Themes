f7bc29a4259ceb8acbc84c50cf1d138f96cfd88b
janak <niraulajanak2019@gmail.com>
2023-08-24 13:24:38 +0545

    Themes: Import Icon Shapes
    Signed-off-by: Janak <niraulajanak2019@gmail.com>

--- themes.mk
+++ themes.mk
@@ -11,3 +11,24 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# Icon shapes
+PRODUCT_PACKAGES += \
+    IconShapeFlowerOverlay \
+    IconShapeHeartOverlay \
+    IconShapeHexagonOverlay \
+    IconShapeCloudyOverlay \
+    IconShapeCylindricalOverlay \
+    IconShapeRiceBallsOverlay \
+    IconShapeStretchedOverlay \
+    IconShapePebbleOverlay \
+    IconShapeRoundedRectOverlay \
+    IconShapeSquircleOverlay \
+    IconShapeTaperedRectOverlay \
+    IconShapeTeardropOverlay \
+    IconShapeVesselOverlay \
+    IconShapeRoundedHexagonOverlay \
+    IconShapeScrollOverlay \
+    IconShapeIosOverlay \
+    IconShapeRohieMeowOverlay \
+    IconShapeLeafyOverlay
